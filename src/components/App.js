24aae865bf4ff9244ea96ec6a3eadc87acf1adbc
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-08 14:00:16 +0300

    another push

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
+import ItemForm from "./ItemForm";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [games, setGames] = useState([]);
 
   function handleDarkModeClick() {
     setIsDarkMode(!isDarkMode);
   }
 
+  function handleAddItem(newGame) {
+    setGames([...games, newGame]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/Searchgame" element={<ShoppingList />} />
+        <Route
+          path="/Addgame"
+          element={<ItemForm onAddItem={handleAddItem} games={games} />}
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,30 +6,28 @@ import Item from "./Item";
 function ShoppingList() {
   const [team, setTeam] = useState("");
   const [games, setGames] = useState([]);
-useEffect(()=>{
-  fetch("http://localhost:4000/games")
-  .then(res=>res.json()
-  .then(games=>setGames(games)))
-},[]
-)
-function handleUpdateItem(updatedItem) {
-  const updatedItems=games.map((game)=>{
-    if(game.id===updatedItem.id){
-      return updatedItem
-    }
-    else{
-      return game
-    }
-  })
-  setGames(updatedItems)
+  useEffect(() => {
+    fetch("http://localhost:3000/games").then((res) =>
+      res.json().then((games) => setGames(games))
+    );
+  }, []);
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = games.map((game) => {
+      if (game.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return game;
+      }
+    });
+    setGames(updatedItems);
   }
   function handleDeleteItem(deletedItem) {
-    const updatedItems=games.filter((game)=>game.id!==deletedItem.id)
-    setGames(updatedItems)
+    const updatedItems = games.filter((game) => game.id !== deletedItem.id);
+    setGames(updatedItems);
   }
-function handleAddItem(newGame) {
-  setGames([...games,newGame]);
-}
+  function handleAddItem(newGame) {
+    setGames([...games, newGame]);
+  }
   function handleCategoryChange(team) {
     setTeam(team);
   }
@@ -40,16 +38,20 @@ function handleAddItem(newGame) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm onAddItem={handleAddItem}
-      games={games}/>
-      <Filter 
+      <ItemForm onAddItem={handleAddItem} games={games} />
+      <Filter
         games={games}
         team={team}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((game) => (
-          <Item key={game.id} game={game} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
+          <Item
+            key={game.id}
+            game={game}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
